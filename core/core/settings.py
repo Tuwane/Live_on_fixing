6a7b96e77badc326f0a42a5815bfc16444f2358f
Tuwane <152686997+Tuwane@users.noreply.github.com>
2025-12-16 01:44:31 +0200

    Fix static files for Render

--- core/core/settings.py
+++ core/core/settings.py
@@ -124,4 +124,5 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.middleware.WhiteNoiseMiddleware', 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+

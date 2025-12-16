a78c0e81af3e6ff96de940acee4ef42d49c11b8a
Tuwane <152686997+Tuwane@users.noreply.github.com>
2025-12-16 23:40:57 +0200

    Updating css on render

--- core/core/settings.py
+++ core/core/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',

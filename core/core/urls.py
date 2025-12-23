d3e9f6b754a748d3d499336e9657981092bba63b
Tuwane <152686997+Tuwane@users.noreply.github.com>
2025-12-23 22:46:59 +0200

    uploading images changed settings

--- core/core/urls.py
+++ core/core/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf.urls.static import static 
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('about/', views.about),
 ]
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
